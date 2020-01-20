85ac0e71afa83b84e3ead98b60b551fa9a53139a
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-01-20 17:30:02 +0800

    api change

--- src/products/Contacts.jsx
+++ src/products/Contacts.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom'
 import { Container, Row, Col, Card, CardGroup, Carousel, Button } from 'react-bootstrap'
 
 const Contacts = ({ contacts }) => {
+
   return (
     <div>
       <center><h1>Contact List</h1></center>
       {
         contacts.map( (contact) => (
           <Card>
-            <Card.Body>
+            <Card.Body key={contact.id}>
               <Card.Title>{contact.name}</Card.Title>
               <Card.Subtitle className="mb-2 text-muted">{contact.email}</Card.Subtitle>
               <Card.Text>{contact.company.catchPhrase}</Card.Text>
