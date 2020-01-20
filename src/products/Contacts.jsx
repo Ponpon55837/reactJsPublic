045f10132b2698ca66116aa5ddc82b20c8f0805b
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-01-20 16:38:28 +0800

    api change

--- src/products/Contacts.jsx
+++ src/products/Contacts.jsx
@@ -6,16 +6,19 @@ const Contacts = ({ contacts }) => {
   return (
     <div>
       <center><h1>Contact List</h1></center>
-      {contacts.map((contact) => (
-        <Card>
-          <Card.Body>
-            <Card.Title>{contact.name}</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">{contact.email}</Card.Subtitle>
-            <Card.Text>{contact.company.catchPhrase}</Card.Text>
-            <Card.Text>{contact.phone}</Card.Text>
-          </Card.Body>
-        </Card>
-      ))}
+      {
+        contacts.map( (contact) => (
+          <Card>
+            <Card.Body>
+              <Card.Title>{contact.name}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">{contact.email}</Card.Subtitle>
+              <Card.Text>{contact.company.catchPhrase}</Card.Text>
+              <Card.Text>{contact.phone}</Card.Text>
+              <Card.Text>{contact.address.street}</Card.Text>
+            </Card.Body>
+          </Card>
+        ))
+      }
     </div>
   )
 }
