a4c237576ab943f87e4623f6b65c2c31387dab45
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-01-24 11:35:13 +0800

    footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,11 +4,16 @@ import { Container, Row, Col} from 'react-bootstrap'
 
 const fatFooter = {
   // position: 'fixed',
-  height: '100px',
+  height: 'auto',
   bottom: '0',
   width: '100%',
   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
   textAlign: 'center',
+}
+
+const fatFooterMargin = {
+  marginTop: '30px',
+  marginBottom: '30px',
 }
 
 class Footer extends Component {
@@ -19,7 +24,13 @@ class Footer extends Component {
   render = () => {
     return (
       <div style={fatFooter}>
-        Copyright by @upfloor5 2020
+        <Container>
+          <Row>
+            <Col></Col>
+            <Col>Copyright by @upfloor5 2020</Col>
+            <Col></Col>
+          </Row>
+        </Container>
       </div>
     )
   }
