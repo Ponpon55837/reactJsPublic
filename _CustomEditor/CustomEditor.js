5ea213775c0dbd04a2b30e82ba22825cb487ad86
upfloor5 <32410486+Ponpon55837@users.noreply.github.com>
2021-07-29 11:33:35 +0800

    update custom editor

--- _CustomEditor/CustomEditor.js
+++ _CustomEditor/CustomEditor.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { CKEditor } from "@ckeditor/ckeditor5-react"
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
-import '@ckeditor/ckeditor5-build-classic/build/translations/zh';
+import '@ckeditor/ckeditor5-build-classic/build/translations/zh'
 import { css } from "@emotion/core"
+
+const editorType = {
+  class: ClassicEditor,
+}
 
 const config = {
   toolbar: [
@@ -14,6 +18,7 @@ const config = {
     "bulletedList",
     "numberedList",
     "blockQuote",
+    "selectAll",
     // "|",
     // "indent",
     // "outdent",
@@ -27,7 +32,48 @@ const config = {
     "undo",
     "redo",
   ],
+  viewportTopOffset: 30,
+  shouldNotGroupWhenFull: true,
   language: 'zh',
+  heading: {
+    options: [
+      {
+        model: 'paragraph',
+        title: 'Paragraph',
+        class: 'ck-heading_paragraph'
+      }, {
+        model: 'heading1',
+        view: 'h1',
+        title: 'Heading 1',
+        class: 'ck-heading_heading1'
+      }, {
+        model: 'heading2',
+        view: 'h2',
+        title: 'Heading 2',
+        class: 'ck-heading_heading2'
+      }, {
+        model: 'heading3',
+        view: 'h3',
+        title: 'Heading 3',
+        class: 'ck-heading_heading3'
+      }, {
+        model: 'heading4',
+        view: 'h4',
+        title: 'Heading 4',
+        class: 'ck-heading_heading4'
+      }, {
+        model: 'heading5',
+        view: 'h5',
+        title: 'Heading 5',
+        class: 'ck-heading_heading5'
+      }, {
+        model: 'heading6',
+        view: 'h6',
+        title: 'Heading 6',
+        class: 'ck-heading_heading6'
+      },
+    ]
+  }
 }
 
 const CustomEditor = ({ defaultData, outputData }) => {
@@ -44,18 +90,25 @@ const CustomEditor = ({ defaultData, outputData }) => {
       z-index: -1;
     }
   `;
+
   return (
     <CKEditor
       css={styles}
-      editor={ClassicEditor}
+      editor={editorType.class}
       data={defaultData}
       config={config}
+      onReady={editor => {
+        editor.ui.view.editable.element.parentElement.insertBefore(
+          editor.ui.view.toolbar.element,
+          editor.ui.view.editable.element
+        );
+      }}
       onChange={(event, editor) => {
         const data = editor.getData();
         outputData(data)
       }}
     />
-  );
+  )
 }
 
 export default CustomEditor
