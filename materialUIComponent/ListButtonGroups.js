90ff0f04ca749184ac322ce9f60b563e92417424
upfloor5 <32410486+Ponpon55837@users.noreply.github.com>
2022-01-07 15:28:27 +0800

    update mui components

--- materialUIComponent/ListButtonGroups.js
+++ materialUIComponent/ListButtonGroups.js
@@ -1,9 +1,13 @@
-import { Button } from '@mui/material'
+import { useState } from 'react'
+import { Typography, Button, Tooltip, ClickAwayListener } from '@mui/material'
 import { makeStyles } from '@mui/styles'
 import PageviewOutlinedIcon from '@mui/icons-material/PageviewOutlined'
 import ModeEditOutlinedIcon from '@mui/icons-material/ModeEditOutlined'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined'
+import CancelIcon from '@mui/icons-material/Cancel'
 import PropTypes from 'prop-types'
+import { CustomBox2 } from '@components/CustomStyle'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -13,6 +17,21 @@ const useStyles = makeStyles(theme => ({
     '& button': {
       marginRight: theme.spacing(1),
       marginBottom: theme.spacing(0.5),
+    },
+  },
+  tooltip: {
+    background: 'rgba(97, 97, 97, 0.92)',
+    color: '#FFFFFF',
+    border: '1px solid rgba(97, 97, 97, 0.92)',
+    borderRadius: '5px',
+  },
+  arrow: {
+    fontSize: 18,
+    width: 17,
+    '&::before': {
+      border: '1px solid rgba(97, 97, 97, 0.92)',
+      backgroundColor: 'rgba(97, 97, 97, 0.92)',
+      boxSizing: 'border-box',
     },
   },
 }))
@@ -27,10 +46,15 @@ const ListButtonGroups = ({
   isCloseDay = false,
   sign = false,
   otherCheck = true,
+  copy = false,
   deleteClick = () => {},
   editClick = () => {},
+  copyClick = () => {},
   viewClick = () => {},
 }) => {
+  const classes = useStyles()
+  const [open, setOpen] = useState(false)
+
   return (
     <ButtonGroup variant="text" color="inherit" aria-label="outlined button group">
       <Button
@@ -64,16 +88,69 @@ const ListButtonGroups = ({
       <Button
         size="small"
         variant="outlined"
-        color="error"
-        startIcon={<DeleteOutlineOutlinedIcon size="small" />}
+        color="success"
+        startIcon={<ContentCopyIcon size="small" />}
         sx={{
           p: '0 !important',
-          display: (status === 0 || status === 3 || isCloseDay || sign) && 'none',
+          display: !copy && 'none',
         }}
-        onClick={() => deleteClick()}
+        onClick={() => copyClick()}
       >
-        刪除
+        複製
       </Button>
+
+      <ClickAwayListener onClickAway={() => setOpen(false)}>
+        <Tooltip
+          classes={{ tooltip: classes.tooltip, arrow: classes.arrow }}
+          arrow
+          placement="top"
+          open={open}
+          title={
+            <CustomBox2>
+              <Typography sx={{ display: 'block', mb: 1 }}>是否刪除此筆資料？</Typography>
+              <Button
+                size="small"
+                variant="contained"
+                color="primary"
+                startIcon={<CancelIcon size="small" />}
+                sx={{
+                  p: '0 !important',
+                  mr: 2,
+                }}
+                onClick={() => setOpen(false)}
+              >
+                取消
+              </Button>
+              <Button
+                size="small"
+                variant="contained"
+                color="error"
+                startIcon={<DeleteOutlineOutlinedIcon size="small" />}
+                sx={{
+                  p: '0 !important',
+                }}
+                onClick={() => deleteClick()}
+              >
+                刪除
+              </Button>
+            </CustomBox2>
+          }
+        >
+          <Button
+            size="small"
+            variant="outlined"
+            color="error"
+            startIcon={<DeleteOutlineOutlinedIcon size="small" />}
+            sx={{
+              p: '0 !important',
+              display: (status === 0 || status === 3 || isCloseDay || sign) && 'none',
+            }}
+            onClick={() => setOpen(true)}
+          >
+            刪除
+          </Button>
+        </Tooltip>
+      </ClickAwayListener>
     </ButtonGroup>
   )
 }
@@ -88,8 +165,10 @@ ListButtonGroups.propTypes = {
   status: PropTypes.number,
   isCloseDay: PropTypes.bool,
   sign: PropTypes.bool,
+  copy: PropTypes.bool,
   otherCheck: PropTypes.bool,
   deleteClick: PropTypes.func,
   editClick: PropTypes.func,
+  copyClick: PropTypes.func,
   viewClick: PropTypes.func,
 }
